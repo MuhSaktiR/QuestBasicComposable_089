8ccbcabe3c31f4c0ef5ce1605535123fc6dba26f
MuhSaktiR <seken876@gmail.com>
2024-10-18 19:56:57 +0700

    Membuat teks Login dan mengatur gaya teks

--- app/src/main/java/com/example/composablelayout/MainActivity.kt
+++ app/src/main/java/com/example/composablelayout/MainActivity.kt
@@ -14,8 +14,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composablelayout.ui.theme.ComposableLayoutTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,7 +46,14 @@ fun BasicCompose(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Top,
         modifier = Modifier.fillMaxWidth().padding(top = 16.dp))
     {
-
+        Text(
+            text = "Login",
+            style = TextStyle(
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Green
+            )
+        )
     }
 }
 
