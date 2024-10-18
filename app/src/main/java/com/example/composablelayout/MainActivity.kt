1e05254beddeb72f39a0f5278a1316b05092300d
MuhSaktiR <seken876@gmail.com>
2024-10-18 20:25:23 +0700

    Menambahkan dan import modifier CircleShape pada image saya

--- app/src/main/java/com/example/composablelayout/MainActivity.kt
+++ app/src/main/java/com/example/composablelayout/MainActivity.kt
@@ -11,11 +11,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -103,7 +105,7 @@ fun BasicCompose(modifier: Modifier = Modifier) {
 
         Image(painter = painterResource(id = R.drawable.saya),
             contentDescription = null,
-            modifier = Modifier.fillMaxWidth().padding(bottom = 60.dp)
+            modifier = Modifier.fillMaxWidth().padding(bottom = 60.dp).clip(CircleShape)
         )// atau ""
     }
 }
